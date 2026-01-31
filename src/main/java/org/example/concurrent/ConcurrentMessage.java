3d6d6299c74ef6855bb57d9aa724e24cfdeda73f
p.kermanshahi <p.kermamshahi@mahsan.co>
2026-01-31 07:14:46 +0330

    impements command pattern

--- src/main/java/org/example/concurrent/ConcurrentMessage.java
+++
@@ -1,8 +0,0 @@
-package org.example.concurrent;
-
-import org.example.constansts.LibraryOperationType;
-
-public abstract class ConcurrentMessage {
-
-    public abstract LibraryOperationType getOperationType();
-}
